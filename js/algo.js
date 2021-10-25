603f821da86031951c32d584fa39f7ef598f725e
kaustubh-vats <kaustubhvats08@gmail.com>
2021-10-25 17:16:49 +0530

    Added Functionality

---
+++ js/algo.js
@@ -0,0 +1,156 @@
+var speed = 550;
+var randomArray = [];
+function setArray(){
+    var mainDiv = document.querySelector('.main-div');
+    randomArray = Array.from({length: arraySize}, () => Math.floor(Math.random() * 500));
+    while (mainDiv.firstChild) {
+        mainDiv.removeChild(mainDiv.lastChild);
+    }
+    for (let i=0;i<arraySize;i++){
+        let bar = document.createElement('div');
+        bar.setAttribute('class','bar');
+        bar.setAttribute('id','bar'+i);
+        bar.style.height = randomArray[i]+"px";
+        bar.style.width = "calc(90vw/"+ arraySize +" - 4px)";
+        mainDiv.appendChild(bar);
+    }
+}
+function changeSpeed(elem){
+    speed = 1100 - Number(elem.value);
+    document.querySelector('#speed').innerHTML=elem.value;
+}
+function beginSorting(elem){
+    if(arraySize == 0){
+        alert('Select the array length');
+        return;
+    }
+    if(algo == '0'){
+        alert('Please Select algorithm to visualize');
+        return;
+    }
+    alert('This part is currently under development\n ALgorithm: '+algo+"\nArray Size: "+arraySize+"\nSpeed: "+speed+"ms/swap");
+    switch(algo){
+        case 'Bubble Sort': bubbleSort(elem);
+                            break;
+        case 'Insertion Sort': insertionSort(elem);
+                                break;
+        case 'Selection Sort': selectionSort(elem);
+                                break;
+    }
+}
+async function selectionSort(elem){
+    elem.setAttribute('onclick','');
+    let n = arraySize;
+    for (let i = 0; i < n-1; i++)
+    {
+        let min_idx = i;
+        for (let j = i + 1; j < n; j++)
+        {
+            if (randomArray[j] < randomArray[min_idx]){
+                min_idx = j;
+            }
+            await selectmin(i,n,j,min_idx);
+        }
+        await myswap(min_idx, i);
+        await setSorted(i,'#75D701');
+        await clearcompare(n,'rgb(201, 201, 201)',i+1)
+    }
+    clearcompare(n,'#75D701');
+    elem.setAttribute('onclick','beginSorting(this)');
+}
+async function selectmin(ind,n,curr,minIdx){
+    for(let i=ind;i<n;i++){
+        let a = document.querySelector("#bar"+i);
+        if(i==minIdx){
+            a.style.background = '#CC8899';
+        }else if(i==curr){
+            a.style.background = '#FCA311';
+        } else {
+            a.style.background = 'rgb(201, 201, 201)';
+        }
+    }
+    await new Promise(resolve => setTimeout(resolve, speed));
+}
+async function bubbleSort(elem){
+    elem.setAttribute('onclick','');
+    let n = arraySize;
+    for(let i = 0 ; i < n - 1 ; ++i) {
+        for(let j = 0 ; j < n - i - 1 ; ++j) {
+            await clearcompare(n-i,'rgb(201, 201, 201)');
+            if(await mycompare(j, j+1)) {
+                await myswap(j, j+1);
+            }
+        }
+        await setSorted(n-1-i,'#75D701');
+    }
+    clearcompare(n,'#75D701');
+    elem.setAttribute('onclick','beginSorting(this)');
+}
+async function insertionSort(elem){
+    elem.setAttribute('onclick','');
+    let n = arraySize;
+    for(let i = 1 ; i < n ; i++) {
+        let key = randomArray[i];
+        let j = i - 1;
+        await setSorted(i,'#42BBFF')
+        while(j >= 0 && randomArray[j] > key) {
+            await setInsertion(j);
+            j -= 1;
+        }
+        await setKey(j+1,key);
+        clearcompare(n,'rgb(201, 201, 201)');
+    }
+    clearcompare(n,'#75D701');
+    elem.setAttribute('onclick','beginSorting(this)');
+}
+async function setKey(i,data){
+    randomArray[i]=data;
+    let a = document.querySelector('#bar'+i);
+    a.style.height = data+"px";
+    a.style.background = '#75D701';
+    await new Promise(resolve => setTimeout(resolve, speed));
+}
+async function setInsertion(j){
+    let a = document.querySelector("#bar"+j);
+    let b = document.querySelector("#bar"+(j+1));
+    randomArray[j+1] = randomArray[j];
+    a.style.background = '#FCA311';
+    b.style.height = randomArray[j]+"px";
+    await new Promise(resolve => setTimeout(resolve, speed));
+}
+async function setSorted(i,color){
+    let a = document.querySelector("#bar"+i);
+    a.style.background = color;
+}
+async function myswap(i,j){
+    let a = document.querySelector("#bar"+i);
+    let b = document.querySelector("#bar"+j);
+    a.style.background = '#FCA311';
+    b.style.background = '#FCA311';
+    let temp = randomArray[i];
+    randomArray[i] = randomArray[j];
+    randomArray[j] = temp;
+    await new Promise(resolve => setTimeout(resolve, speed));
+    a.style.height = randomArray[i]+"px";
+    b.style.height = randomArray[j]+"px";
+    a.style.background = '#42BBFF';
+    b.style.background = '#42BBFF';
+    await new Promise(resolve => setTimeout(resolve, speed));
+}
+async function mycompare(i,j){
+    let a = document.querySelector("#bar"+i);
+    let b = document.querySelector("#bar"+j);
+    a.style.background = 'yellow';
+    b.style.background = 'yellow';
+    await new Promise(resolve => setTimeout(resolve, speed));
+    if(randomArray[i] > randomArray[j]){
+        return true;
+    }
+    return false;
+}
+async function clearcompare(n,color,init=0){
+    for(let i=init;i<n;i++){
+        let a = document.querySelector("#bar"+i);
+        a.style.background = color;
+    }
+}
--- js/header.js
+++ js/header.js
@@ -1,3 +1,5 @@
+var algo = '0';
+var arraySize = 0;
 const navSlide = () =>{
     const burger = document.querySelector('.burger');  
     const nav = document.querySelectorAll('.lis');
@@ -15,6 +17,15 @@ const navSlide = () =>{
     });
 }
 function selectthis(e, params){
+    if(params == 'algorithms') {
+        algo = e.innerHTML;
+        document.querySelector('#algoName').innerHTML = e.innerHTML;
+        setDetails(e.innerHTML);
+    }
+    else {
+        arraySize = Number(e.innerHTML);
+        setArray();
+    }
     document.querySelector('.'+params+' button').innerHTML = e.innerHTML+' <i class="fas fa-angle-down"></i>';
     document.querySelector("."+params+" ul").classList.toggle('visible-links');
     document.querySelector("."+params+" ul").classList.toggle('invisible-links');
@@ -22,5 +33,31 @@ function selectthis(e, params){
 function showDropDown(params){
     document.querySelector("."+params+" ul").classList.toggle('visible-links');
     document.querySelector("."+params+" ul").classList.toggle('invisible-links');
+}
+function setDetails(algo){
+    let bubble = "Average Case Time Complexity: O(n <sup>2</sup>) <br /> Best Case Time Complexity: O(n) <br />Worst Case Time Complexity:  O(n <sup>2</sup>)  <br />Space Complexity: O(1) <br />";
+    let insertion = "Average Case Time Complexity:  O(n <sup>2</sup>) <br /> Best Case Time Complexity: O(n) <br />Worst Case Time Complexity:  O(n <sup>2</sup>)  <br />Space Complexity: O(1) <br />";
+    let selection = "Average Case Time Complexity:  O(n <sup>2</sup>) <br /> Best Case Time Complexity:  O(n <sup>2</sup>)  <br />Worst Case Time Complexity:  O(n <sup>2</sup>)  <br />Space Complexity: O(1) <br />";
+    let quick = "Average Case Time Complexity: O(n logn) <br /> Best Case Time Complexity: O(n logn) <br />Worst Case Time Complexity:  O(n <sup>2</sup>)  <br />Space Complexity: O(log n) <br />";
+    let bubbleCode = "\nvoid bubbleSort(int arr[], int n){\n    int i, j;\n    for (i = 0; i < n-1; i++)\n    for (j = 0; j < n-i-1; j++)\n        if (arr[j] > arr[j+1])\n            swap(&arr[j], &arr[j+1]);\n}"
+    let insertionCode = "\n    void insertionSort(int arr[], int n)\n    {\n        int i, key, j;\n        for (i = 1; i < n; i++)\n        {\n            key = arr[i];\n            j = i - 1;\n     \n            /* Move elements of arr[0..i-1], that are\n            greater than key, to one position ahead\n            of their current position */\n            while (j >= 0 && arr[j] > key)\n            {\n                arr[j + 1] = arr[j];\n                j = j - 1;\n            }\n            arr[j + 1] = key;\n        }\n    }"
+    let selectionCode ="\n    void selectionSort(int arr[], int n)\n    {\n        int i, j, min_idx;\n     \n        // One by one move boundary of unsorted subarray\n        for (i = 0; i < n-1; i++)\n        {\n            // Find the minimum element in unsorted array\n            min_idx = i;\n            for (j = i+1; j < n; j++)\n            if (arr[j] < arr[min_idx])\n                min_idx = j;\n     \n            // Swap the found minimum element with the first element\n            swap(&arr[min_idx], &arr[i]);\n        }\n    }"
+    let quickCode = "\nint partition (int arr[], int low, int high) \n{ \n    int pivot = arr[high]; // pivot \n    int i = (low - 1);\n    for (int j = low; j <= high - 1; j++) \n    { \n        // If current element is smaller than the pivot \n        if (arr[j] < pivot) \n        { \n            i++; // increment index of smaller element \n            swap(&arr[i], &arr[j]); \n        } \n    } \n    swap(&arr[i + 1], &arr[high]); \n    return (i + 1); \n}\nvoid quickSort(int arr[], int low, int high) \n{ \n    if (low < high) \n    { \n        /* pi is partitioning index, arr[p] is now \n        at right place */\n        int pi = partition(arr, low, high); \n        // Separately sort elements before \n        // partition and after partition \n        quickSort(arr, low, pi - 1); \n        quickSort(arr, pi + 1, high); \n    } \n}";
+    let complexityDiv = document.querySelector('#complexity');
+    let codeDiv = document.querySelector('#code');
+    switch(algo){
+        case "Bubble Sort": complexityDiv.innerHTML = bubble;
+                            codeDiv.innerHTML = hljs.highlight(bubbleCode, {language: 'cpp'}).value;
+                            break;
+        case "Insertion Sort": complexityDiv.innerHTML = insertion;
+                                codeDiv.innerHTML = hljs.highlight(insertionCode, {language: 'cpp'}).value;
+                                break;
+        case "Selection Sort":complexityDiv.innerHTML = selection;
+                                codeDiv.innerHTML = hljs.highlight(selectionCode, {language: 'cpp'}).value;
+                                break;
+        case "Quick Sort": complexityDiv.innerHTML = quick;
+                            codeDiv.innerHTML = hljs.highlight(quickCode, {language: 'cpp'}).value;
+                            break;
+    }
 }
 navSlide();
